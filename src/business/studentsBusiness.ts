d6de4dbb5c6ace1d23905f620cf8ebdc79e0dee7
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-11-23 20:58:18 -0300

    feat(business): creating StudentsBusiness class and signup method

---
+++ src/business/studentsBusiness.ts
@@ -0,0 +1,40 @@
+import { studentsDataBase } from "../data/StudentsDataBase";
+import { inputNewStudent, Student } from "../model/Student";
+import Authenticator from "../services/Authenticator";
+import { hashManage } from "../services/HashManage";
+import IdGenerator from "../services/IdGenerator";
+
+class StudentsBusiness {
+    public async signup (signup:inputNewStudent): Promise<string> {
+        try {
+    
+            if (!signup.email || !signup.password) {
+                throw new Error('"email" and "password" must be provided')
+            }
+       
+            const cypherPassword: string = await hashManage.hash(signup.password);
+    
+            const student: Student = new Student(
+                IdGenerator.generateId(),
+                signup.email,
+                cypherPassword
+            )
+    
+            await studentsDataBase.createStudent(student)
+       
+            const token: string = Authenticator.generateToken({id: student.getId()})
+            if(!token) throw new Error("Invalid token");
+    
+            return token
+    
+        } catch (error) {
+            if(error.sqlMessage.includes("Duplicate entry")){
+                throw new Error("Registered student")
+            }
+            throw new Error(error)
+        }
+    }
+
+}
+
+export default new StudentsBusiness()
