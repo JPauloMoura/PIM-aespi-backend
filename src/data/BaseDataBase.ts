ed92ee2ecd48e6f72324429c4a69020e6b9d8471
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-11-23 02:36:01 -0300

    fix(BaseDataBase):changing the database port

--- src/data/BaseDataBase.ts
+++ src/data/BaseDataBase.ts
@@ -9,7 +9,7 @@ export default class BaseDataBase {
         client: "pg",
         connection: {
            host: process.env.DB_HOST,
-           port: 3306,
+           port: 5432,
            user: process.env.DB_USER,
            password: process.env.DB_PASSWORD,
            database: process.env.DB_NAME
