49a4a7c1c234ae8f7ddc64ae403b0a94733d6c59
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-11-23 20:17:01 -0300

    feat(model): creating Students class

---
+++ src/modal/Students.ts
@@ -0,0 +1,13 @@
+export class Students {
+    constructor(
+       private id: string,
+       private email: string,
+       private password: string,
+       private questionnaireAnswered: boolean
+    ){ }
+
+    public getId = () => this.id
+    public getEmail = () => this.email
+    public getPassword = () => this.password
+    public getQuestionnaireAnswered = () => this.questionnaireAnswered
+}
