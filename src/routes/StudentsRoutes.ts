3a99498d5db186997e02300b4a6487d0a82071a6
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-11-25 20:47:00 -0300

    refacto: deleted outdated files

--- src/routes/StudentsRoutes.ts
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import StudentsController from "../controller/StudentsController";
-
-export const studentsRouter = express.Router();
-
-studentsRouter.post("/signup", StudentsController.signup)
-studentsRouter.post("/login", StudentsController.login)
