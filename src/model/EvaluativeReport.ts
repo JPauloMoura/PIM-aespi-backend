9143fe0eeb293555ed01f0ad3db6ecb695d9f907
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-11-26 21:37:22 -0300

    feat(model): create class Evaluative Report

---
+++ src/model/EvaluativeReport.ts
@@ -0,0 +1,39 @@
+export class EvaluativeReport {
+    constructor(
+       private idQuestion: string,
+       private response: TypeResponse,
+    ){
+        switch (response.toLowerCase()) {
+            case TypeResponse.GREAT:
+                this.response = TypeResponse.GREAT
+                break;
+            case TypeResponse.GOOD:
+                this.response = TypeResponse.GOOD
+                break
+            case TypeResponse.BAD:
+                this.response = TypeResponse.BAD
+                break
+            case TypeResponse.VERY_BAD:
+                this.response = TypeResponse.VERY_BAD
+                break
+            default:
+                throw new Error("Invalid typeResponse: choose 'great', 'good', 'bad' or 'very bad'");
+        }
+     }
+
+    public getIdQuestion = () => this.idQuestion
+    public getResponse = () => this.response
+}
+
+export enum TypeResponse {
+  GREAT = "great",
+  GOOD = "good",
+  BAD = "bad",
+  VERY_BAD = "very bad"
+}
+
+export interface inputResponseQuestion {
+    id: string,
+    typeResponse: TypeResponse,
+    token: string
+}
