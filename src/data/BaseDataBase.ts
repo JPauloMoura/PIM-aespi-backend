c413732e2ff10145a0d16bc4621d653f4560b00b
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-11-30 00:02:13 -0300

    rename .env

--- src/data/BaseDataBase.ts
+++ src/data/BaseDataBase.ts
@@ -10,7 +10,7 @@ export abstract class BaseDataBase {
                 client: "pg",
                 connection: {
                 host: process.env.DB_HOST,
-                port: Number(process.env.DP_PORT),
+                port: Number(process.env.DB_PORT),
                 user: process.env.DB_USER,
                 password: process.env.DB_PASSWORD,
                 database: process.env.DB_NAME,
